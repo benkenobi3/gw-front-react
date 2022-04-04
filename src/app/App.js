c4480cec027e1b9dcf7400b0cd74154c09cb7042
benkenobi3 <vezde.max@gmail.com>
2022-04-04 19:17:09 +0300

    folders

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Row, Col} from 'antd'
+
+function App() {
+  return (
+    <div>
+      <Row>
+        <Col span={12}>col-12</Col>
+        <Col span={12}>col-12</Col>
+      </Row>
+    </div>
+  );
+}
+
+export default App;
